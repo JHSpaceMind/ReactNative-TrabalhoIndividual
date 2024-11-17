59d47d068653dfe84ae75197c0cf0c53358cef56
juliahx0 <juliaxavier.134@gmail.com>
2024-11-17 03:27:20 -0300

    trabalho arduo

---
+++ src/components/Footer.tsx
@@ -0,0 +1,60 @@
+import { Text, View, StyleSheet, Image } from "react-native";
+import Entypo from '@expo/vector-icons/Entypo';
+import AntDesign from '@expo/vector-icons/AntDesign';
+
+export const Footer = () => {
+    return (
+        <View style={styles.container}>
+            <Entypo 
+               style={styles.icon}
+                name="home" 
+                size={24} 
+                color="black" 
+            />
+            <AntDesign 
+               style={styles.icon}
+                name="search1" 
+                size={24}
+                color="black" 
+            />
+            <Image
+                style={styles.icon}
+                source={require('../../assets/icones/publi.png')} 
+            />
+            <Image
+                style={styles.icon}
+                source={require('../../assets/icones/reels.png')} 
+            />
+            <Image 
+                style={styles.user}
+                source={require('../../assets/person/user.jpg')} 
+            />
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        position: 'absolute',
+        bottom: 0,
+        width: '100%',
+        paddingVertical: 10,
+        backgroundColor: '#fff', 
+        borderTopWidth: 1,
+        borderTopColor: '#ddd',
+    },
+    icon: {
+        height: 28,
+        width: 28,
+        marginHorizontal: 8,
+    },
+    user: {
+        borderRadius: 30,
+        height: 28,
+        width: 28,
+        marginHorizontal: 8,
+    },
+});
---
+++ src/components/NavBar.tsx
@@ -0,0 +1,61 @@
+import { useFonts } from 'expo-font';
+import { Text, View, StyleSheet, Image } from "react-native";
+import AntDesign from '@expo/vector-icons/AntDesign';
+
+export const Navbar = () => {
+    let [fontsLoaded] = useFonts({
+        'Settikef-PersonalUse': require('../../assets/fonts/Settikef-PersonalUse.otf'),
+    });
+
+    if (!fontsLoaded) {
+        return <Text>Carregando...</Text>;
+    }
+
+    return (
+        <View style={styles.container}>
+            <Text style={styles.TextLog}> Instagram </Text>
+            <View style={styles.iconContainer}>
+                <AntDesign
+                    style={styles.iconHeart}
+                    name="hearto"
+                    size={24} 
+                    color="black" 
+                />
+                <Image
+                    style={styles.iconChat}
+                    source={require('../../assets/icones/chat.png')}
+                />
+            </View>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    container: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'flex-start',
+       
+    },
+    TextLog: {
+        fontFamily: 'Settikef-PersonalUse', 
+        fontSize: 27,
+        color: '#000',
+        paddingTop: 28,
+        paddingRight: 180,
+    },
+    iconContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        paddingTop: 28,
+    },
+    iconChat: {
+        width: 31,
+        height: 31,
+        paddingBottom:10,
+        marginLeft: 9,
+    },
+    iconHeart: {
+        marginLeft: 20,
+    },
+});
